3ff7fe4be82884fc2ec9c600cf986eec6225e1a9
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-05-03 14:32:45 +0000

    create BusinessCard App

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const BusinessCard());
+}
+
+class BusinessCard extends StatelessWidget {
+  const BusinessCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
